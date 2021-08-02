dc9891f3a64ceb10f01d13e3bf5ec54689167340
Aidas777 <67761030+Aidas777@users.noreply.github.com>
2021-08-02 10:11:09 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+
+        html {
+            background-color: rgb(232, 232, 232);
+            margin: 0 auto;
+        }
+
+        h4 {
+            width: 100%;
+            text-align: center;
+            font-style: italic;
+            background-color: lightgrey;
+
+        }
+
+    </style>
+
+
+</head>
+<body>
+
+<h4>1. Uzduotis</h4>
+<!-- /////////////////////////////////////////////////// 1 /////////////////////////////////////////////////// -->
+
+<?php
+// 
+
+?>
+
+    
+</body>
+</html>
