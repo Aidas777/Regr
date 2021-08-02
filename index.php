caa6c652dc67a9bd6198c5141994d93297dca52a
Aidas777 <67761030+Aidas777@users.noreply.github.com>
2021-08-02 10:54:04 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Testines uzduotys</title>
 
     <style>
 
@@ -31,8 +31,45 @@
 <!-- /////////////////////////////////////////////////// 1 /////////////////////////////////////////////////// -->
 
 <?php
-// 
+// Sukurkite masyvą iš 101 elemento, kurio indeksai yra nuo 0 iki 100.
+// Tiems masyvo nariams, kurių indeksas dalijasi iš 10 (0, 10, 20, 30,...100), priskirkite reikšmę “A”.
+// Likusiems nariams priskirkite reikšmę “B”. Masyvą atspausdinkite naršyklės lange naudodami funkciją print_r();
 
+
+for ($i=0; $i <100 ; $i++) { 
+    if ( $i % 10 == 0 ) {
+        $arrMain[] = "A";
+    } else {
+        $arrMain[] = "B";
+    }
+}
+
+print_r($arrMain);
+// // Arba galim atvaizduoti budu zemiau:
+$arrMainLen=count($arrMain);
+// for ($i=0; $i < $arrMainLen; $i++) { 
+//    echo $i ."=" .$arrMain[$i] ." ";
+// }
+
+echo "<br>";
+// /////////////////////////////////////////////////// 2 /////////////////////////////////////////////////// 
+
+for ($i=0; $i < $arrMainLen; $i++) { 
+    // $rndLen=rand(3, 10);
+    for ($y=0; $y < rand(3, 10) ; $y++) { 
+        if ($arrMain[$i] == "A") {
+            $arrIn[$y]="A";
+        } else {
+            $arrIn[$y]="B";
+        }
+    }
+    $arrMain[$i]=$arrIn;
+}
+
+//PRITUKAU LAIKO ...
+
+print_r($arrMain);
+// var_dump($arrMain);
 ?>
 
     
